633b03dd706b8c85835be3f72bdcf28a3bb295ec
harshv5904 <harshv5094@gmail.com>
2023-11-03 13:27:05 +0530

    feat(api): Creating a basic express app with db

--- db.js
+++ db.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose')
-const env = require('dotenv').config()
+require('dotenv').config()
 
 const mongoURI = process.env.MONGODBURI
 
 mongoose
-  .connect(mongoURI, {
-    useUnifiedTopology: true
-  })
+  .connect(mongoURI)
   .then(() => console.log('Database is successfully connected to mongoDB'))
   .catch(error => console.log('Error Connecting Application:\n', error))
--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 require('./db')
 const express = require('express')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Server is running on port ${process.env.PORT}</h1>`)
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`)
+})
